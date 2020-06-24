a1b1dc55fcfef680fe0c8874acc50f254217d236
Sam <srsuddath@gmail.com>
2020-06-23 22:44:20 -0400

    Update starting point

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Sam Suddath's Portfolio`,
+    description: `This is a all in one information hub for everything you need to know about Sam Suddath `,
+    author: `@SolonTheWizard`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
