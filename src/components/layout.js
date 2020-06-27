feabdffc2d181801d561033bdbdefbf5c0503266
Sam <srsuddath@gmail.com>
2020-06-27 10:42:37 -0400

    Create Business Card layout

---
+++ src/components/layout.js
@@ -0,0 +1,18 @@
+import React from "react"
+import PropTypes from "prop-types"
+
+import "./layout.css"
+
+const Layout = ({ children }) => {
+  return (
+    <div className="background">
+      <main>{children}</main>
+    </div>
+  )
+}
+
+Layout.propTypes = {
+  children: PropTypes.node.isRequired,
+}
+
+export default Layout
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,20 +1,26 @@
 import React from "react"
 import { Link } from "gatsby"
 
+import Layout from "../components/layout"
 import "../components/index.css"
 import SEO from "../components/seo"
 
 const IndexPage = () => (
-  <div className="background">
+  <Layout>
     <SEO title="Home" />
     <div className="business-card">
+      <div className="external-links">
+        <Link to="/portfolio/">Github Link Here</Link>
+      </div>
       <h1>Samuel Suddath</h1>
-      <h2>SolonTheWizard</h2>
-      <Link to="/portfolio/">Portfolio</Link> <br />
-      <Link to="/resume/">Resume</Link> <br />
-      <Link to="/contract/">Contact</Link>
+      <h3>SolonTheWizard</h3>
+      <div className="internal-links">
+        <Link to="/portfolio/">Portfolio</Link>
+        <Link to="/resume/">Resume</Link>
+        <Link to="/contract/">Contact</Link>
+      </div>
     </div>
-  </div>
+  </Layout>
 )
 
 export default IndexPage
