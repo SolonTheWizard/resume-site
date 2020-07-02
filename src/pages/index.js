5f761367dfc2566675ce49bb1e391c495d47e3f9
Sam <srsuddath@gmail.com>
2020-07-01 20:19:46 -0400

    Rework things with Kelson

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,42 +1,118 @@
 import React from "react"
 import { Link } from "gatsby"
+import styled from "styled-components"
 
-import "../components/index.css"
-import SEO from "../components/seo"
+import "../components/globalStyles.css"
+import SEO from "../global/seo"
+import Layout from "../global/layout"
 
 const IndexPage = () => (
-  <div className="background">
+  <Layout>
     <SEO title="Home" />
-    <div className="business-card">
-      <div className="external-links">
-        <a href="https://github.com/SolonTheWizard">Github</a>
-        <a href="https://www.linkedin.com/in/samuel-s-548841120/">LinkedIN</a>
+    <Wrapper>
+      <div className="business-card">
+        <div className="external-links">
+          <a href="https://github.com/SolonTheWizard">Github</a>
+          <a href="https://www.linkedin.com/in/samuel-s-548841120/">LinkedIN</a>
+        </div>
+        <div className="info">
+          <h1 className="title">Samuel Suddath</h1>
+          <h3 className="subtitle">Front-End React Developer</h3>
+        </div>
+        <div className="internal-links">
+          <Link to="/portfolio/" className="directory-link">
+            Portfolio
+          </Link>
+          <Link to="/resume/" className="directory-link">
+            Resume
+          </Link>
+          <Link to="/contact/" className="directory-link">
+            Contact
+          </Link>
+        </div>
       </div>
-      <div className="info">
-        <h1 className="title">Samuel Suddath</h1>
-        <h3 className="subtitle">Front-End React Developer</h3>
-      </div>
-      <div className="internal-links">
-        <Link to="/portfolio/" className="directory-link">
-          Portfolio
-        </Link>
-        <Link to="/resume/" className="directory-link">
-          Resume
-        </Link>
-        <Link to="/contact/" className="directory-link">
-          Contact
-        </Link>
-      </div>
-    </div>
-  </div>
+    </Wrapper>
+  </Layout>
 )
+const Wrapper = styled.div`
+  min-width: 100%;
+  min-height: 100%;
+  margin: 0;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: space-around;
+  flex-grow: 1;
 
+  .business-card {
+    box-shadow: 5px 10px 23px rgba(31, 28, 87, 0.2);
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    padding: 20px 30px;
+    margin-bottom: 80px;
+    margin-top: 180px;
+    width: 600px;
+  }
+
+  .internal-links {
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    align-items: center;
+    margin: 8px 0 10px;
+  }
+  .external-links {
+    display: flex;
+    flex-direction: row-reverse;
+    justify-content: flex-start;
+    min-width: 100%;
+  }
+
+  .directory-link:hover {
+    background-color: #f6f6f6;
+    transition: ease 0.3s;
+    -webkit-box-shadow: 0px 1px 3px rgba(0, 0, 0, 0.15);
+    -moz-box-shadow: 0px 1px 3px rgba(0, 0, 0, 0.15);
+    box-shadow: 0px 1px 3px rgba(0, 0, 0, 0.15);
+  }
+  .menu-bar-link {
+    padding: 5px 0px;
+    margin: 0 0 15px;
+    text-align: center;
+  }
+
+  .title {
+    padding: 10px 0;
+    margin: 2px 4px 2px;
+    font-size: 50px;
+    font-weight: 600;
+    /* ask kelson to help me change the font*/
+  }
+  .subtitle {
+    padding: 0;
+    margin: 0 0 8px;
+    font-size: 34px;
+    font-weight: 400;
+    /* ask kelson to help me change the font*/
+  }
+
+  .info {
+    display: flex;
+    flex-direction: column;
+    align-items: flex-start;
+    width: 600px;
+    margin: 0 0 10px;
+  }
+`
 export default IndexPage
 
 /*
 Questions:
 1) How to do images
 2) How to change fonts
-3) Why is my index.css getting overwritten by page.css
-4)
+3) Why is my index.css getting overwritten by 
+4) cool background for home screen
+5) make links to external pages as images
+6) favicon in white?
 */
--- src/pages/portfolio.js
+++ src/pages/portfolio.js
@@ -1,86 +1,220 @@
 import React from "react"
 import { Link } from "gatsby"
+import styled from "styled-components"
 
-import "../components/page.css"
-import SEO from "../components/seo"
+import project1Image from "../images/project1Image.png"
+import SEO from "../global/seo"
+import Layout from "../global/layout"
 
 const PortfolioPage = () => (
-  <div className="background">
+  <Layout>
     <SEO title="Portfolio" />
-    <div className="menu-bar">
-      <Link to="/" className="menu-bar-link">
-        Home
-      </Link>
-      <Link to="/portfolio/" className="selected-page menu-bar-link">
-        __Portfolio__
-      </Link>
-      <Link to="/resume/" className="menu-bar-link">
-        Resume
-      </Link>
-    </div>
-    <div className="title">
-      <h1>Portfolio</h1>
-    </div>
-    <div className="project">
-      <div className="project-details">
-        <div className="project-title">
-          <h2>Custom Spellbook</h2>
-        </div>
-        <div className="project-tags">
-          <span>React</span>
-          <span>JavaScript</span>
-          <span>Node.js</span>
-          <span>Styled Components</span>
-        </div>
-        <div className="description">
-          An easy way for a DM or Player to keep track of their chosen spells or
-          create new ones for {"D&D"} 5e
-        </div>
-        <ul>
-          <li>Built using Node.js and React</li>
-          <li>Hosted using Google Firebase</li>
-          <li>Utilizes Local Storage to save user data and custom spells</li>
-          <li>
-            Registers and Stores multiple users in a home-made
-            user-authentication system complete with password update and
-            password reset capabilities
-          </li>
-          <li>
-            Each registered user has their own unique "book" of spells that they
-            can modify, add, or delete spells in
-          </li>
-          <li>
-            When spells are deleted they are temporarily stored for an easy
-            "quick-restore" feature
-          </li>
-        </ul>
+    <Wrapper>
+      <div className="menu-bar">
+        <Link to="/" className="menu-bar-link">
+          Home
+        </Link>
+        <Link to="/portfolio/" className="selected-page menu-bar-link">
+          __Portfolio__
+        </Link>
       </div>
-      <div className="project-image">Image goes here</div>
-    </div>
-    <div className="project">
-      <div className="project-image">Image goes here</div>
-      <div className="project-details">
-        <div className="project-title">
-          <h2>Dad Joke Hangman</h2>
-        </div>
-        <div className="project-tags">
-          <span>React</span>
-          <span>JavaScript</span>
-          <span>Firebase</span>
-          <span>Public API</span>
-        </div>
-        <div className="description">
-          A "punny" game of hangman where the prompts are all dad jokes!
-        </div>
-        <ul>
-          <li> Detail 1</li>
-          <li> Detail 2</li>
-          <li> Detail 3</li>
-          <li> Detail 4</li>
-        </ul>
+      <div className="title">
+        <h1>Portfolio</h1>
       </div>
-    </div>
-  </div>
+      <div className="project">
+        <div className="project-details">
+          <div className="project-title">
+            <h2>Custom Spellbook</h2>
+          </div>
+          <div className="project-tags">
+            <span>React</span>
+            <span>JavaScript</span>
+            <span>Node.js</span>
+            <span>Styled Components</span>
+          </div>
+          <div className="description">
+            An easy way for a DM or Player to keep track of their chosen spells
+            or create new ones for {"D&D"} 5e
+          </div>
+          <ul>
+            <li>Built using Node.js and React</li>
+            <li>Hosted using Google Firebase</li>
+            <li>Utilizes Local Storage to save user data and custom spells</li>
+            <li>
+              Registers and Stores multiple users in a home-made
+              user-authentication system complete with password update and
+              password reset capabilities
+            </li>
+            <li>
+              Each registered user has their own unique "book" of spells that
+              they can modify, add, or delete spells in
+            </li>
+            <li>
+              When spells are deleted they are temporarily stored for an easy
+              "quick-restore" feature
+            </li>
+          </ul>
+        </div>
+        <div className="project-image">
+          <a
+            href="https://example.com"
+            target="_blank"
+            rel="noreferrer noopener"
+          >
+            <img src={project1Image} alt="project 1" height="500" width="500" />
+          </a>
+        </div>
+      </div>
+      <div className="project">
+        <div className="project-image">Image goes here</div>
+        <div className="project-details">
+          <div className="project-title">
+            <h2>Dad Joke Hangman</h2>
+          </div>
+          <div className="project-tags">
+            <span>React</span>
+            <span>JavaScript</span>
+            <span>Firebase</span>
+            <span>Public API</span>
+          </div>
+          <div className="description">
+            A "punny" game of hangman where the prompts are all dad jokes!
+          </div>
+          <ul>
+            <li> Detail 1</li>
+            <li> Detail 2</li>
+            <li> Detail 3</li>
+            <li> Detail 4</li>
+          </ul>
+        </div>
+      </div>
+    </Wrapper>
+  </Layout>
 )
+
+const Wrapper = styled.div`
+  display: flex;
+  min-width: 100%;
+  min-height: 100%;
+  margin: 0;
+  flex-direction: column;
+  align-items: center;
+  justify-content: space-around;
+  font-size: 16px;
+
+  .menu-bar {
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    justify-content: center;
+    padding: 40px 10px 10px;
+    width: 100%;
+    flex-grow: 1;
+  }
+
+  a {
+    padding: 5px 8px;
+    margin: 0 5px 15px;
+    color: black;
+    text-decoration: none;
+    width: 90px;
+    text-align: center;
+  }
+
+  h1 {
+    padding-left: 75px;
+    margin: 0 0 2px 10px;
+    font-size: 34px;
+    font-weight: 400;
+    letter-spacing: 1px;
+    word-spacing: 5px;
+  }
+  h2 {
+    margin: 0 0 2px 10px;
+    font-size: 34px;
+    font-weight: 300;
+    letter-spacing: 1px;
+    word-spacing: 5px;
+  }
+
+  .title {
+    display: flex;
+    flex-direction: row;
+    align-items: flex-start;
+    width: 100%;
+    font-family: ;
+  }
+
+  .project {
+    display: flex;
+    flex-direction: row;
+    justify-content: space-around;
+    margin-left: 20px;
+    padding: 10px 10px;
+    width: 90%;
+    height: 500px;
+    margin-bottom: 50px;
+  }
+
+  .project-details {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    width: 50%;
+  }
+
+  .project-image {
+    display: flex;
+    width: 500px;
+    height: 500px;
+    margin: 10px;
+    align-items: center;
+    justify-content: center;
+    -webkit-box-shadow: 0px 6px 6px rgba(0, 0, 0, 0.15);
+    -moz-box-shadow: 0px 6px 6px rgba(0, 0, 0, 0.15);
+    box-shadow: 0px 6px 6px rgba(0, 0, 0, 0.15);
+  }
+
+  .project-tags {
+    display: flex;
+    width: 100%;
+    flex-direction: row;
+    align-items: center;
+    justify-content: flex-start;
+  }
+
+  .project-title {
+    display: flex;
+    width: 100%;
+    justify-content: flex-start;
+    text-decoration: underline;
+    margin-top: 15px;
+  }
+
+  .selected-page {
+    text-decoration: underline;
+  }
+
+  span {
+    padding: 5px 15px;
+    background-color: black;
+    color: white;
+    font-size: 14px;
+    margin: 0 10px 10px;
+  }
+
+  .description {
+    display: flex;
+    flex-direction: row;
+    justify-content: flex-start;
+    align-items: center;
+    width: 100%;
+    padding-left: 20px;
+  }
+
+  li {
+    margin-bottom: 6px;
+  }
+`
 
 export default PortfolioPage
